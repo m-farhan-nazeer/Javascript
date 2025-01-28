1f97dfc93afd900e090db48a4ba96887d34eec3e
bsdsf22A037 <bsdsf22a037@pucit.edu.pk>
2025-01-28 06:11:57 +0500

    equality and bitwise operators

--- js_1.js
+++ js_1.js
@@ -6,3 +6,20 @@ let a = 5;
 let b = 7;
 console.log(a + b )
 console.log(a ** b)
+
+
+//                                              loose equality
+
+console.log('5' == 5 )
+
+//                                                         strict equality
+console.log('5' === 5 )
+
+
+                // bitwise_operators
+
+console.log( 5 & 2)
+console.log(5|2)
+console.log(~5)
+console.log(5>>1)
+console.log(5<<1)
