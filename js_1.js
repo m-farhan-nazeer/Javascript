79b0bc1ed772fafe3b542d0934a6046e9583bce1
bsdsf22A037 <bsdsf22a037@pucit.edu.pk>
2025-01-28 06:23:24 +0500

    switch

--- js_1.js
+++ js_1.js
@@ -2,47 +2,59 @@
 
 //                                                      Binary operators
 
-let a = 5;
-let b = 7;
-console.log(a + b )
-console.log(a ** b)
+// let a = 5;
+// let b = 7;
+// console.log(a + b )
+// console.log(a ** b)
 
 
-//                                                       loose equality
+// //                                                       loose equality
 
-console.log('5' == 5 )
+// console.log('5' == 5 )
 
-//                                                        strict equality
-console.log('5' === 5 )
+// //                                                        strict equality
+// console.log('5' === 5 )
 
 
-                //                                        bitwise_operators
+//                 //                                        bitwise_operators
 
-console.log( 5 & 2)
-console.log(5|2)
-console.log(~5)
-console.log(5>>1)
-console.log(5<<1)
-//                                                       conditions
+// console.log( 5 & 2)
+// console.log(5|2)
+// console.log(~5)
+// console.log(5>>1)
+// console.log(5<<1)
+// //                                                       conditions
 
-let age = 8;
-if (age >18){
-    console.log('can vote')
+// let age = 8;
+// if (age >18){
+//     console.log('can vote')
+// }
+// else{
+//     console.log('cannot vote')
+// }
+// let num = 3;
+// if (num ==1){
+//     console.log('a')
+// }
+// else if(num == 0 ){
+//     console.log('b')
+// }
+// else if (num == 2){
+//     console.log('c')
+// }
+// else{
+//     console.log('f')
+// }
+//                                                 // switch 
+
+let number = 2;
+switch(number){
+    case 1 : console.log('a')
+    break;
+    case 2 : console.log('b')
+    break;
+    case 3 : console.log('c')
+    break;
+    default : console.log ('f')
+
 }
-else{
-    console.log('cannot vote')
-}
-let num = 3;
-if (num ==1){
-    console.log('a')
-}
-else if(num == 0 ){
-    console.log('b')
-}
-else if (num == 2){
-    console.log('c')
-}
-else{
-    console.log('f')
-}
-                                  
