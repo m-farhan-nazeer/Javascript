e7dd820050086a261caf723824322d68ab6dcba4
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-09-22 21:59:06 +0500

    ternanary

---
+++ js_1.js
@@ -0,0 +1,92 @@
+// console.log("hello World");
+
+//                                                      Binary operators
+
+// let a = 5;
+// let b = 7;
+// console.log(a + b )
+// console.log(a ** b)
+
+
+// //                                                       loose equality
+
+// console.log('5' == 5 )
+
+// //                                                        strict equality
+// console.log('5' === 5 )
+
+
+//                 //                                        bitwise_operators
+
+// console.log( 5 & 2)
+// console.log(5|2)
+// console.log(~5)
+// console.log(5>>1)
+// console.log(5<<1)
+// //                                                       conditions
+
+// let age = 8;
+// if (age >18){
+//     console.log('can vote')
+// }
+// else{
+//     console.log('cannot vote')
+// }
+// let num = 3;
+// if (num ==1){
+//     console.log('a')
+// }
+// else if(num == 0 ){
+//     console.log('b')
+// }
+// else if (num == 2){
+//     console.log('c')
+// }
+// else{
+//     console.log('f')
+// }
+//                                                 // switch 
+
+// let number = 2;
+// switch(number){
+//     case 1 : console.log('a')
+//     break;
+//     case 2 : console.log('b')
+//     break;
+//     case 3 : console.log('c')
+//     break;
+//     default : console.log ('f')
+
+// }
+
+
+// ------------------------------------------------string literals--------------------------------------------
+
+
+
+// let age = `my age is ${5+5+5}`
+// console.log(age)
+
+
+// --------------------------Truthness and Falsiness----------------------
+// let user = 'farhan';
+// if (user){
+//     console.log("welcome");
+// }
+// else{
+//     console.log("no user added ")
+// }
+
+
+let num = 7;
+num === 7 ? console.log('lucky') : console.log("not lucky");
+
+let statuss = 'ofline';
+let color = statuss ==='ofline'? 'red' : 'green';
+console.log(color)
+
+
+
+
+
+
